a35fc7bb97a117fbde62b59a0b0f3a71f91f7114
Alicia Thompson <alicia@Alicias-MacBook-Pro.local>
2019-09-22 23:17:00 -0700

    first commit for app

---
+++ Resources/js/main.js
@@ -0,0 +1,10 @@
+
+//when user clicked on the add button
+//function should save the item in the item field to the todo list
+
+document.getElementById('add').addEventListener('click', function() {
+  var fieldValue= document.getElementById('item').value;
+  if (fieldValue){
+    console.log('')
+  }
+}
